b19031dfb849f469b3001c679cb7ac4cc4506018
Abel Silva <abel.silva@gmail.com>
2017-09-05 18:36:40 -0300

    change src to a docker image taking snapshot every minute

--- src/arlo-snapshot.py
+++ src/arlo-snapshot.py
@@ -1,7 +1,7 @@
 from Arlo import Arlo
 
-USERNAME = 'user@example.com'
-PASSWORD = 'supersecretpassword'
+USERNAME = os.environ['USERNAME']
+PASSWORD = os.environ['PASSWORD']
 
 try:
     # Instantiating the Arlo object automatically calls Login(), which returns an oAuth token that gets cached.
@@ -15,15 +15,6 @@ try:
     # Filter on device type to only get the cameras.
     # This will return an array which includes all of the cameras and their associated metadata.
     cameras = [ device for device in devices if device['deviceType'] == 'camera' ]
-
-    # Starting recording with a camera.
-    arlo.StartRecording(cameras[0]['parentId'], cameras[0]['deviceId'], cameras[0]['xCloudId'], cameras[0]['properties']['olsonTimeZone']);
-
-    # Wait for 4 seconds while the camera records. (There are probably better ways to do this, but you get the idea.)
-    time.sleep(4)
-
-    # Stop recording.
-    arlo.StopRecording(cameras[0]['parentId'], cameras[0]['deviceId'], cameras[0]['xCloudId'], cameras[0]['properties']['olsonTimeZone']);
 
     # Take the snapshot.
     arlo.TakeSnapshot(cameras[0]['parentId'], cameras[0]['deviceId'], cameras[0]['xCloudId'], cameras[0]['properties']['olsonTimeZone']);
---
+++ src/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+while true
+do
+  python arlo-snapshot.py
+  sleep 60
+done
